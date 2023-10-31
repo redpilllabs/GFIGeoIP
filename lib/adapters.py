615ba8dd2000da35e4f22572db9a62b72b7563d1
Kyōchikutō | キョウチクトウ <kyochikuto@proton.me>
2023-11-01 01:17:21 +0330

    Improve Excel adapter

--- lib/adapters.py
+++ lib/adapters.py
@@ -1,10 +1,14 @@
+from io import StringIO
+
 import pandas as pd
 from bs4 import BeautifulSoup
+
+from lib.cidr_utils import extract_to_ipv4_ipv6
 
 
 def convert_xls_to_df(xls_path: str):
     """
-    Loads Excel sheet from ITO, extracts the CIDRs of
+    Loads an Excel sheet from ITO, extracts the CIDRs of
     the Iranian intranet into a DataFrame
 
     Args:
@@ -13,18 +17,22 @@ def convert_xls_to_df(xls_path: str):
     Returns:
         DataFrame: DataFrame containing CIDR of the Iranian intranet.
     """
-    ito_df = pd.DataFrame()
     with open(xls_path) as xml_file:
         soup = BeautifulSoup(xml_file.read(), "html.parser")
-        ito_df = pd.read_html(soup.decode_contents())[0]
+        html_string = soup.decode_contents()
+        string_io_object = StringIO(html_string)
+        ito_df = pd.read_html(string_io_object)[0]
 
-    # Websites' DB and messengers' DB have different column names! Typical IR!
-    if "IP" in ito_df.columns:
-        ito_df = ito_df.rename(columns={"IP": "Network"})
-    elif "IPv4" in ito_df.columns:
-        ito_df = ito_df.rename(columns={"IPv4": "Network"})
+        # Websites' DB and messengers' DB have different column names. Typical IR!
+        if "IP" in ito_df.columns:
+            ito_df = ito_df.rename(columns={"IP": "Network"})
+        elif "IPv4" in ito_df.columns:
+            ito_df = ito_df.rename(columns={"IPv4": "Network"})
 
-    ito_df = ito_df[["Network"]]
-    ito_df["Tag"] = "IR"
+        # Drop every column except 'Network'
+        ito_df = ito_df[["Network"]]
+        ito_df["Tag"] = "IR"
 
-    return ito_df
+        ipv4, ipv6 = extract_to_ipv4_ipv6(ito_df)
+
+        return ipv4, ipv6
