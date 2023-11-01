3de742b49ce39bb5531251eb31e25a0933395019
Kyōchikutō | キョウチクトウ <kyochikuto@proton.me>
2023-11-01 04:58:47 +0330

    Remove downloading functions for Github workflows migration

--- lib/fetchers.py
+++ lib/fetchers.py
@@ -35,28 +35,3 @@ def fetch_remote_ip_list(url: str, network_tag: str, proxies=None):
         df = pd.DataFrame(ip_list, columns=["Network"])
         df["Tag"] = network_tag
     return df
-
-
-def download_and_unzip_geoip_database(license_key, output_file):
-    """Downloads and unzips a GeoIP database from MaxMind.
-
-    Args:
-      license_key: The MaxMind license key.
-      output_file: The path to the output file.
-    """
-
-    # Download the GeoIP database.
-    url = f"https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV&license_key={license_key}&suffix=zip"
-    response = requests.get(url)
-    response.raise_for_status()
-
-    # Save the GeoIP database to a file.
-    with open(output_file, "wb") as f:
-        f.write(response.content)
-
-    # Unzip the GeoIP database.
-    with zipfile.ZipFile(output_file, "r") as zip_file:
-        zip_file.extractall()
-
-    # Remove the GeoIP database ZIP file.
-    os.remove(output_file)
